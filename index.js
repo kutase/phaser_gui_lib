cd58ce1f7cd93050970a4cbee668938cc419fc98
Nikita Puzyrev <kutase123@gmail.com>
2017-05-05 20:37:00 +0500

    fix

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-function init (game) {
-
-	// console.log('Current game:', game)
-	let setGame = require('./Game').setGame
-
-	setGame(game)
-
-	return () => ({
-		AbstractGraphic: require('./AbstractGraphic').default,
-		AbstractComponent: require('./AbstractComponent').default,
-		AbstractLeaf: require('./AbstractLeaf').default,
-		Button: require('./Button').default,
-		Container: require('./Container').default,
-		Image: require('./Image').default,
-		Input: require('./Input').default,
-		Text: require('./Text').default,
-		RootContainer: require('./RootContainer').default
-	})
+module.exports = {
+	AbstractGraphic: require('./AbstractGraphic').default,
+	AbstractComponent: require('./AbstractComponent').default,
+	AbstractLeaf: require('./AbstractLeaf').default,
+	Button: require('./Button').default,
+	Container: require('./Container').default,
+	Image: require('./Image').default,
+	Input: require('./Input').default,
+	Text: require('./Text').default,
+	RootContainer: require('./RootContainer').default,
 }
-
-module.exports = init
